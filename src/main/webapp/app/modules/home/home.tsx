423b8af86f74b5a3ab3e3f9a51cc72a9ba5398c6
schatzopoulos <s.chatzopoulos@gmail.com>
2020-03-11 16:43:51 +0200

    Add select for entity and attributes

--- src/main/webapp/app/modules/home/home.tsx
+++ src/main/webapp/app/modules/home/home.tsx
@@ -2,90 +2,185 @@ import './home.scss';
 
 import React from 'react';
 import { Link } from 'react-router-dom';
-
 import { connect } from 'react-redux';
-import { Row, Col, Alert } from 'reactstrap';
-
+import { 
+	Row, 
+	Col, 
+	InputGroup, 
+	InputGroupAddon, 
+	Input, 
+	Button, 
+	Spinner,
+	ListGroup,
+	Progress,
+	Container,
+	Card, 
+  CardBody,
+  Form,
+  FormGroup,
+  Label,
+} from 'reactstrap';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { IRootState } from 'app/shared/reducers';
+import  _  from 'lodash';
+import AutocompleteInput from '../autocomplete/autocomplete-input';
 
-export type IHomeProp = StateProps;
-
-export const Home = (props: IHomeProp) => {
-  const { account } = props;
-
-  return (
-    <Row>
-      <Col md="9">
-        <h2>Welcome, Java Hipster!</h2>
-        <p className="lead">This is your homepage</p>
-        {account && account.login ? (
-          <div>
-            <Alert color="success">You are logged in as user {account.login}.</Alert>
-          </div>
-        ) : (
-          <div>
-            <Alert color="warning">
-              If you want to
-              <Link to="/login" className="alert-link">
-                {' '}
-                sign in
-              </Link>
-              , you can try the default accounts:
-              <br />- Administrator (login=&quot;admin&quot; and password=&quot;admin&quot;)
-              <br />- User (login=&quot;user&quot; and password=&quot;user&quot;).
-            </Alert>
-          </div>
-        )}
-        <p>If you have any question on JHipster:</p>
-
-        <ul>
-          <li>
-            <a href="https://www.jhipster.tech/" target="_blank" rel="noopener noreferrer">
-              JHipster homepage
-            </a>
-          </li>
-          <li>
-            <a href="http://stackoverflow.com/tags/jhipster/info" target="_blank" rel="noopener noreferrer">
-              JHipster on Stack Overflow
-            </a>
-          </li>
-          <li>
-            <a href="https://github.com/jhipster/generator-jhipster/issues?state=open" target="_blank" rel="noopener noreferrer">
-              JHipster bug tracker
-            </a>
-          </li>
-          <li>
-            <a href="https://gitter.im/jhipster/generator-jhipster" target="_blank" rel="noopener noreferrer">
-              JHipster public chat room
-            </a>
-          </li>
-          <li>
-            <a href="https://twitter.com/jhipster" target="_blank" rel="noopener noreferrer">
-              follow @jhipster on Twitter
-            </a>
-          </li>
-        </ul>
-
-        <p>
-          If you like JHipster, do not forget to give us a star on{' '}
-          <a href="https://github.com/jhipster/generator-jhipster" target="_blank" rel="noopener noreferrer">
-            Github
-          </a>
-          !
-        </p>
-      </Col>
-      <Col md="3" className="pad">
-        <span className="hipster rounded" />
-      </Col>
-    </Row>
-  );
+export interface IHomeProps extends StateProps, DispatchProps {
+	loading: boolean;
+	progress: number;
+	progressMsg: string;
+	error: string;
+	docs: any;
+	meta: any;
+	uuid: string;
 };
 
-const mapStateToProps = storeState => ({
-  account: storeState.authentication.account,
-  isAuthenticated: storeState.authentication.isAuthenticated
+export class Home extends React.Component<IHomeProps> {
+	readonly state: any = { 
+    entity: 'Article',
+    attribute: 'id',
+		dataset: "gdelt",
+	};
+	polling: any;
+	entities: any = [
+    { "name": "Article", "attributes": [ { "name": "id", "type": "numeric" }, { "name": "url", "type": "string" }, { "name": "pos", "type": "numeric" }, { "name": "neg", "type": "numeric" } ]},
+    { "name": "Person", "attributes": [ { "name": "id", "type": "numeric" }, { "name": "name", "type": "string" } ] },
+    { "name": "Organisation", "attributes": [ { "name": "id", "type": "numeric" } , { "name": "name", "type": "string" } ] }, 
+    { "name": "Theme", "attributes": [ { "name": "id", "type": "numeric" } , { "name": "name", "type": "string" } ] },
+    { "name": "Source", "attributes": [ { "name": "id", "type": "numeric" } , { "name": "name", "type": "string" } ] },
+    { "name": "Location", "attributes": [ { "name": "id", "type": "numeric" } , { "name": "name", "type": "string" }, { "name": "code", "type": "string" }, { "name": "lat", "type": "numeric" }, { "name": "lon", "type": "numeric" } ] },
+  ];
+
+	pollForResults() {
+		// this.polling = setInterval( () => {
+		// 	this.props.getResults(this.props.analysis, this.props.uuid);
+		// }, 1000);
+	}
+
+	componentDidUpdate(prevProps) {
+
+		// new uuid detected, start polling
+		if (this.props.loading && !prevProps.loading) {
+			this.pollForResults();
+		} else if (prevProps.loading && !this.props.loading) {
+			clearInterval(this.polling);
+		}
+	}
+
+	componentWillUnmount() {
+		clearInterval(this.polling);
+	}
+
+	componentDidMount() {
+		
+	}
+
+	execute(e) {
+    e.preventDefault();
+    // console.log("execute sim search");
+	}
+
+	loadMoreResults() {
+		// this.props.getMoreResults(this.props.analysis, this.props.uuid, this.props.meta.page + 1);
+  }
+
+  handleSourceNode(e) {
+
+  }
+
+  handleSourceEntity(e) {
+    e.preventDefault();
+    this.setState({
+      entity: e.target.value
+    });
+  }
+
+  handleSourceAttribute(e) {
+    e.preventDefault();
+    this.setState({
+      attribute: e.target.value
+    });
+  }
+
+  getSourceEntityOptions() {
+    return this.entities.map( (entity, index) => {
+      return <option key={index} value={entity.name}>{entity.name}</option>;
+    });
+  }
+
+  getSourceAttributeOptions() {
+    const entity = _.find(this.entities, [ 'name', this.state.entity]);
+    return entity.attributes.map( (attribute, index) => {
+      return <option key={index} value={attribute.name}>{attribute.name}</option>;
+    });
+  }
+
+	render() {
+   
+		return (
+			<Container fluid>
+        <Row>
+          <Col md="6">
+            <Form>
+              <FormGroup>
+                <Label for="source_entity"><h4>1. Select source entity</h4></Label>
+                <Input  id="source_entity" value={this.state.entity} type="select" name="source_entity" onChange={this.handleSourceEntity.bind(this)}>
+                    {this.getSourceEntityOptions()}
+                  </Input>
+              </FormGroup>
+              <FormGroup>
+                <Label for="source_attribute"><h4>2. Select key attribute for {this.state.entity}</h4></Label>
+                <Input  id="source_attribute" value={this.state.attribute} type="select" name="source_attribute" onChange={this.handleSourceAttribute.bind(this)}>
+                    {this.getSourceAttributeOptions()}
+                </Input>
+              </FormGroup>
+              <FormGroup>
+                <Label for="source_node"><h4>3. Select source node</h4></Label>
+                <AutocompleteInput 
+                  id="targetEntityInput"
+                  placeholder={''}
+                  onChange={this.handleSourceNode.bind(this)}								
+                  entity={this.state.entity}
+                  field={this.state.attribute}
+                  folder={this.state.dataset}
+                />
+              </FormGroup>
+            </Form>
+          </Col>
+        </Row>
+			</Container>
+		);
+	}
+};
+
+const mapStateToProps = (storeState: IRootState) => ({  
+	// loading: storeState.analysis.loading,
+	// progress: storeState.analysis.progress,
+	// progressMsg: storeState.analysis.progressMsg,
+	// error: storeState.analysis.error,
+	// docs: storeState.analysis.docs,
+	// meta: storeState.analysis.meta,
+	// uuid: storeState.analysis.uuid,  
+	// analysis: storeState.analysis.analysis,
+	// schemas: storeState.datasets.schemas,
 });
 
-type StateProps = ReturnType<typeof mapStateToProps>;
+const mapDispatchToProps = { 
+	// rankingRun, 
+	// simjoinRun,
+	// simsearchRun,
+	// getResults,
+	// getMoreResults,
+	// getDatasetSchemas,
+};
 
-export default connect(mapStateToProps)(Home);
+type StateProps = ReturnType<typeof mapStateToProps>;
+type DispatchProps = typeof mapDispatchToProps;
+
+export default connect(
+	mapStateToProps,
+	mapDispatchToProps,
+)(Home);
+
+
+
